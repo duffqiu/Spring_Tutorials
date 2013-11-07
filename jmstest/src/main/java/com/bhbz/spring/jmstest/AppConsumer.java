789b43ae6540c7a2e852e1cd1502447a34109963
Duff Qiu <duffqiu@qq.com>
2013-11-07 20:10:08 +0800

    create project

---
+++ jmstest/src/main/java/com/bhbz/spring/jmstest/AppConsumer.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.bhbz.spring.jmstest;
+
+/**
+ * @author macbook
+ *
+ * 2013年11月7日
+ */
+public class AppConsumer {
+
+    /**
+     * @param args
+     * macbook
+     * 2013年11月7日
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ jmstest/src/main/java/com/bhbz/spring/jmstest/AppProducer.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.bhbz.spring.jmstest;
+
+/**
+ * @author macbook
+ *
+ * 2013年11月7日
+ */
+public class AppProducer {
+
+}
